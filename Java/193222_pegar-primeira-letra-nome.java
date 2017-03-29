ee63b6f8d91db76c2a557e13cf80cbfccc6ec6c8
Igor Venturelli <igor.venturelli@me.com>
2017-03-29 09:05:29 -0300

    Added 193222_pegar-primeira-letra-nome
    Added 193222_pegar-primeira-letra-nome

---
+++ Java/193222_pegar-primeira-letra-nome.java
@@ -0,0 +1,11 @@
+//http://pt.stackoverflow.com/questions/193222/pegar-a-primeira-letra-do-nome-e-do-%C3%BAltimo-sobrenome
+
+public class Teste {
+	public static void main(String[] args) {
+		String nome = "Carlos";
+		String primeiraLetra = Character.toString(nome.charAt(0));
+		int posicaoUltimoEspaco = nome.lastIndexOf(" ");
+		String primeiraLetraUltimoNome = nome.substring(posicaoUltimoEspaco + 1, posicaoUltimoEspaco + 2);
+		System.out.println(primeiraLetra + " " + primeiraLetraUltimoNome);
+	}
+}
